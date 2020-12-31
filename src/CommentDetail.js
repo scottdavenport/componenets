0ab47ad8437135548b51c26a477b170001e23c16
scottdavenport <scott@pinpoint.com>
2020-12-31 12:49:27 -0500

    Passing Props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -4,16 +4,16 @@ const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src="https://source.unsplash.com/random" />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00pm</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text"> Nice blog post! </div>
+        <div className="text">{props.text}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:23PM"
+        text="Great blog!"
+        avatar="https://source.unsplash.com/random"
+      />
       <hr></hr>
-      <CommentDetail author="Alex" />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        text="It really is nicely done!"
+        avatar="https://source.unsplash.com/random"
+      />
       <hr></hr>
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 1:53PM"
+        text="I tend to disagree."
+        avatar="https://source.unsplash.com/random"
+      />
     </div>
   );
 };
