a3e697bdf7bbf9ce5953c42fcc74da8b201ff318
scottdavenport <scott@pinpoint.com>
2020-12-31 18:27:43 -0500

    Implementing Approval Card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:23PM"
