e365e0e1efab6107d5504cd4c8a777c8e5e17efd
scottdavenport <scott@pinpoint.com>
2020-12-31 18:49:13 -0500

    Component Reuse

--- src/index.js
+++ src/index.js
@@ -7,26 +7,38 @@ const App = () => {
 	return (
 		<div className="ui container comments">
 			<ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    timeAgo="Today at 4:23PM"
-                    content="Great blog!"
-                    avatar="https://source.unsplash.com/random"
-                />
-            </ApprovalCard>
-			<CommentDetail
-				author="Alex"
-				timeAgo="Today at 2:00AM"
-				content="It really is nicely done!"
-				avatar="https://source.unsplash.com/random"
-			/>
-			<hr></hr>
-			<CommentDetail
-				author="Jane"
-				timeAgo="Yesterday at 1:53PM"
-				content="I tend to disagree."
-				avatar="https://source.unsplash.com/random"
-			/>
+				<div>
+					<h4>Warning</h4>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:23PM"
+					content="Great blog!"
+					avatar="https://source.unsplash.com/random"
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					content="It really is nicely done!"
+					avatar="https://source.unsplash.com/random"
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 1:53PM"
+					content="I tend to disagree."
+					avatar="https://source.unsplash.com/random"
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
