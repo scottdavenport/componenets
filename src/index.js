9454eba7c49b56b28d749dc0c6029da12e6a7de6
scottdavenport <scott@pinpoint.com>
2020-12-31 18:36:11 -0500

    prettier configuration

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:23PM"
-        content="Great blog!"
-        avatar="https://source.unsplash.com/random"
-      />
-      <hr></hr>
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        content="It really is nicely done!"
-        avatar="https://source.unsplash.com/random"
-      />
-      <hr></hr>
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 1:53PM"
-        content="I tend to disagree."
-        avatar="https://source.unsplash.com/random"
-      />
-    </div>
-  );
+	return (
+		<div className="ui container comments">
+			<ApprovalCard />
+			<CommentDetail
+				author="Sam"
+				timeAgo="Today at 4:23PM"
+				content="Great blog!"
+				avatar="https://source.unsplash.com/random"
+			/>
+			<hr></hr>
+			<CommentDetail
+				author="Alex"
+				timeAgo="Today at 2:00AM"
+				content="It really is nicely done!"
+				avatar="https://source.unsplash.com/random"
+			/>
+			<hr></hr>
+			<CommentDetail
+				author="Jane"
+				timeAgo="Yesterday at 1:53PM"
+				content="I tend to disagree."
+				avatar="https://source.unsplash.com/random"
+			/>
+		</div>
+	);
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
