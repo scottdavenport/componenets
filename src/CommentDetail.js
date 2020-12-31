6b91c17c118a8f90b86690882e594da6e2fddef7
scottdavenport <scott@pinpoint.com>
2020-12-31 12:44:01 -0500

    make it prettier

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CommentDetail = props => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -8,7 +8,7 @@ const CommentDetail = props => {
       </a>
       <div className="content">
         <a href="/" className="author">
-            {props.author}
+          {props.author}
         </a>
         <div className="metadata">
           <span className="date">Today at 6:00pm</span>
