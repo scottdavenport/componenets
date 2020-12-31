a6c8e0a05e37d6d055e149fb198cb22426ff8b99
scottdavenport <scott@pinpoint.com>
2020-12-31 12:09:32 -0500

    Specifying Images in JSX

--- src/index.js
+++ src/index.js
@@ -6,7 +6,41 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src='https://source.unsplash.com/random' />
                 </a>
                 <div className="content">
                     <a href="/" className="author">
